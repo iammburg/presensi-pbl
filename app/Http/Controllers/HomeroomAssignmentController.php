def251b06207c66a9c2bc065f4a0844121e75781
iammburg <ilhamprabowo009@gmail.com>
2025-04-28 21:44:09 +0700

    feat: implement HomeroomAssignment resource with CRUD operations and views

---
+++ app/Http/Controllers/HomeroomAssignmentController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HomeroomAssignment;
+use Illuminate\Http\Request;
+
+class HomeroomAssignmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HomeroomAssignment $homeroomAssignment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(HomeroomAssignment $homeroomAssignment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, HomeroomAssignment $homeroomAssignment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HomeroomAssignment $homeroomAssignment)
+    {
+        //
+    }
+}
