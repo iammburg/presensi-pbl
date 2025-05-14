992f2be35ae6141a7040e7c4fc8394dff78dff62
Prima <novap6278@gmail.com>
2025-05-14 13:12:21 +0700

    membuat tampilan kelola pelanggaran

--- resources/views/manage-violations/index.blade.php
+++ resources/views/manage-violations/index.blade.php
@@ -1,7 +1,115 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <h1>Kelola Pelanggaran</h1>
-</div>
+@section('title')
+    Kelola Pelanggaran
 @endsection
+
+@push('css')
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+@endpush
+
+@section('content')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6 text-uppercase">
+                    <h4 class="m-0">Kelola Pelanggaran</h4>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        {{-- Tambahkan breadcrumb jika diperlukan --}}
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-primary card-outline">
+                        <div class="card-header">
+                            <h3 class="card-title">Data Pelanggaran</h3>
+                            <div class="card-tools">
+                                <a href="#" class="btn btn-primary">
+                                    <i class="fas fa-plus-circle"></i> Tambah Data
+                                </a>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table id="datatable-main" class="table table-bordered table-striped">
+                                    <thead class="bg-tertiary text-white">
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama Pelanggaran</th>
+                                            <th>Jenis Pelanggaran</th>
+                                            <th>Poin</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                            <tr>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td>
+                                                    <div class="btn-group">
+                                                        <button type="button"
+                                                            class="btn btn-sm btn-outline-info dropdown-toggle"
+                                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                            <i class="fas fa-cog"></i>
+                                                        </button>
+                                                        <div class="dropdown-menu">
+                                                            {{-- <a class="dropdown-item"
+                                                                href="{{ route('manage-academic-years.edit', $academicYear->id) }}">Edit</a>
+                                                            <form action="{{ route('manage-academic-years.destroy', $academicYear->id) }}" --}}
+                                                                {{-- method="POST"
+                                                                onsubmit="return confirm('Yakin ingin menghapus data ini?')">
+                                                                @csrf --}}
+                                                                @method('DELETE')
+                                                                <button type="submit"
+                                                                    class="dropdown-item text-danger">Hapus</button>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        {{-- @endforeach --}}
+                                    </tbody>
+                                </table>
+                            </div> {{-- table-responsive --}}
+                        </div> {{-- card-body --}}
+                    </div> {{-- card --}}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('js')
+    <!-- DataTables Scripts -->
+    <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+
+    {{-- <script>
+        $(function () {
+            $('#datatable-main').DataTable({
+                responsive: true,
+                autoWidth: false,
+                language: {
+                    url: "//cdn.datatables.net/plug-ins/1.10.25/i18n/Indonesian.json"
+                }
+            });
+        });
+    </script> --}}
+@endpush
