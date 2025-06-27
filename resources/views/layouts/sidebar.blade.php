9f87b1e05571ce073dfcb1c912a5e7034cdf4044
Umu Afifah <umuafifah393@gmail.com>
2025-06-27 16:46:29 +0700

    fix: icon circle nya tadi lurus

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -62,7 +62,7 @@
                             <li class="nav-item text-sm">
                                 <a href="{{ $isPoinPrestasi ? route('student.achievements') : ($isPoinPelanggaran ? route('student.violations') : url($endmenu->url)) }}"
                                     class="nav-link {{ (Request::url() == route('student.achievements') && $isPoinPrestasi) || (Request::url() == route('student.violations') && $isPoinPelanggaran) || Request::segment(1) == $endmenu->url ? 'active' : '' }}">
-                                    <i class="far fa-circle"></i>
+                                    <i class="nav-icon far fa-circle"></i>
                                     <p>{{ ucwords($endmenu->nama_menu) }}</p>
                                 </a>
                             </li>
